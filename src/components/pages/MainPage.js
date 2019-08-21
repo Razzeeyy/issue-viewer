c8945c484c06761463b59a64a3dd59dda335f130
Alexey Shmakov <razzeeyy@gmail.com>
2019-08-21 23:38:51 +0300

    remove pagination component completely

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -2,16 +2,12 @@ import React from 'react'
 
 import RepoSearchContainer from '../containers/RepoSearchContainer'
 import IssuesListContainer from '../containers/IssuesListContainer'
-import Pagination from '../presentational/Pagination'
 
 export default function MainPage(props) {
     return (
         <div>
             <RepoSearchContainer />
             <IssuesListContainer />
-            {
-                //<Pagination totalPages={5} selectedPage={2} onClickPage={(page) => console.log(page)}/>
-            }
         </div>
     )
 }
