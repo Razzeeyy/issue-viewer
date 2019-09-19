29aaaf239ef25c3d3509afbd2ce0c3f869867af4
Alexey Shmakov <razzeeyy@gmail.com>
2019-09-19 18:54:46 +0300

    restyle loading indicator

--- src/components/presentational/LoadingIndicator.js
+++ src/components/presentational/LoadingIndicator.js
@@ -1,11 +1,19 @@
 import React from 'react'
 
-export default function LoadingIndicator({
-    isLoading
-}) {
+const style = {
+    position: 'fixed',
+    left: 0,
+    top: 0,
+    display: 'block',
+    width: '100%',
+    height: 2,
+    background: 'black'
+}
+
+export default function LoadingIndicator({ isLoading }) {
     return (
         <>
-            {isLoading && <div>loading</div>}
+            {isLoading && <div style={style}></div>}
         </>
     )
 }
