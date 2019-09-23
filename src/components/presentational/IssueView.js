f2fe6a38337ec976a5f71bdebdb9b51e77e25cc1
Alexey Shmakov <razzeeyy@gmail.com>
2019-09-23 12:32:32 +0300

    style issue view

--- src/components/presentational/IssueView.js
+++ src/components/presentational/IssueView.js
@@ -1,13 +1,18 @@
 import React from 'react'
+import './IssueView.css'
 
 export default function IssueView({ issue }) {
     return (
-        <div>
-            {issue.title || 'Untitled'}
-            <br/>
-            {issue.body || 'No body'}
-            <br/>
-            {(issue.author && issue.author) || 'Unknown author'}
+        <div className="issue-view">
+            <h1 className="issue-view__title">
+                {issue.title || 'Untitled'}
+            </h1>
+            <div className="issue-view__author">
+                {issue.author ? issue.author : 'Unknown author'}
+            </div>
+            <div className="issue-view__body">
+                {issue.body || 'No body'}
+            </div>
         </div>
     )
 }
