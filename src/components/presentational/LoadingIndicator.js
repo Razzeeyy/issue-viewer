833319770df2e042a427e757ccebd7e1b2cdcd21
Alexey Shmakov <razzeeyy@gmail.com>
2019-09-23 17:12:53 +0300

    animate loading indicator

--- src/components/presentational/LoadingIndicator.js
+++ src/components/presentational/LoadingIndicator.js
@@ -2,9 +2,17 @@ import React from 'react'
 import './LoadingIndicator.css'
 
 export default function LoadingIndicator({ isLoading }) {
+    const indicatorClassNames = [
+        'loading-indicator',
+        !isLoading && 'loading-indicator--hidden',
+    ].filter(x => !!x).join(' ')
+    const sliderClassNames = [
+        'loading-indicator__slider',
+        isLoading && 'loading-indicator__slider--animate'
+    ].filter(x => !!x).join(' ')
     return (
-        <>
-            {isLoading && <div className="loading-indicator"></div>}
-        </>
+        <div className={indicatorClassNames}>
+            <div className={sliderClassNames} />
+        </div>
     )
 }
